7e9afc921d4f4ae0450fe9608a95889620dc959b
SerhiiiihreS <0992830717s@gmail.com>
2025-04-24 11:20:43 +0200

    RegistUser

--- GameFlow/Data/DataAccessor.cs
+++ GameFlow/Data/DataAccessor.cs
@@ -1,22 +1,77 @@
 ﻿using GameFlow.Services.KDF;
+using GameFlow.Data.Entities;
+using GameFlow.Migrations;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+
+
 
 namespace GameFlow.Data;
 
-public class DataAccessor
+public class DataAccessor(DataContext dataContext, IHttpContextAccessor httpContextAccessor, IKDFService kdfService)
 {
-    private readonly DataContext _dataContext;
-    private readonly IHttpContextAccessor _httpContextAccessor;
-    private readonly IKDFService _kdfService;
+    private readonly DataContext _dataContext = dataContext;
+    private readonly IHttpContextAccessor _httpContextAccessor = httpContextAccessor;
+    private readonly IKDFService _kdfService = kdfService;
 
-    public DataAccessor(DataContext dataContext, IHttpContextAccessor httpContextAccessor, IKDFService kdfService)
+    private string ImagePath =>
+        $"{_httpContextAccessor.HttpContext?.Request.Scheme}://{_httpContextAccessor.HttpContext?.Request.Host}/Shop/Image";
+
+    public GameFlow.Data.Entities.AccessToken Authenticate(HttpRequest Request)
     {
-        _dataContext = dataContext;
-        _httpContextAccessor = httpContextAccessor;
-        _kdfService = kdfService;
-    }
+        String authHeader = Request.Headers.Authorization.ToString();
+        if (String.IsNullOrEmpty(authHeader))
+        {
+            throw new Win32Exception(401, "Authorization header required");
+        }
+        String scheme = "Basic ";
+        if (!authHeader.StartsWith(scheme))
+        {
+            throw new Win32Exception(401, $"Authorization scheme must be {scheme}");
+        }
+        String credentials = authHeader[scheme.Length..];
+        String authData;
+        try
+        {
+            authData = System.Text.Encoding.UTF8.GetString(
+                Base64UrlTextEncoder.Decode(credentials)
+            );
+        }
+        catch
+        {
+            throw new Win32Exception(401, $"Not valid Base64 code '{credentials}'");
+        }
+        // authData == "login:password"  
+        String[] parts = authData.Split(':', 2);
+        if (parts.Length != 2)
+        {
+            throw new Win32Exception(401, $"Not valid credentials format (missing ':'?)");
+        }
+        String login = parts[0];
+        String password = parts[1];
+        var userAccess = _dataContext.UserAccesses.FirstOrDefault(ua => ua.Login == login);
+        if (userAccess == null)
+        {
+            throw new Win32Exception(401, "Введіть логін!");
+        }
+        if (_kdfService.DerivedKey(password, userAccess.Salt) != userAccess.Dk && _kdfService.DerivedKey(password, userAccess.Salt) != null)
+        {
+            throw new Win32Exception(401, "Введіть пароль!");
+        }
 
-    private string ImagePath => 
-        $"{_httpContextAccessor.HttpContext.Request.Scheme}://{_httpContextAccessor.HttpContext.Request.Host}/Shop/Image";
+        GameFlow.Data.Entities.AccessToken accessToken = new()
+        {
+            Jti = Guid.NewGuid(),
+            Sub = userAccess.Id,
+            Aud = userAccess.UserId,
+            Iat = DateTime.Now,
+            Nbf = null,
+            Exp = DateTime.Now.AddMinutes(10),
+            Iss = "GameFlow"
+        };
+        _dataContext.AccessTokens.Add(accessToken);
+        _dataContext.SaveChanges();
+        return accessToken;
+    }
 }
---
+++ GameFlow/Data/Entities/AccessToken.cs
@@ -0,0 +1,13 @@
+﻿namespace GameFlow.Data.Entities
+{
+    public class AccessToken
+    {
+        public Guid Jti { get; set; }
+        public Guid Sub { get; set; }
+        public Guid Aud { get; set; }
+        public DateTime Iat { get; set; } = DateTime.Now;
+        public DateTime? Nbf { get; set; }
+        public DateTime Exp { get; set; }
+        public String? Iss { get; set; }
+    }
+}
